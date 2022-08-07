ea8302dc284750c4cdd563d3c1c6b5f5566b1ae6
Taiju Sanagi <taijusanagi@gmail.com>
2022-08-07 14:20:05 +0900

    added basic livepeer selection

--- packages/frontend/src/components/Main/Main.tsx
+++ packages/frontend/src/components/Main/Main.tsx
@@ -1,27 +1,68 @@
-import {
-  Box,
-  Button,
-  Divider,
-  Stack,
-  Text,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Box, Button, Stack, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
 
+import { Mode } from "../../types/livepeer";
 import { ConnectWalletWrapper } from "../ConnectWalletWrapper";
 
 export const Main: React.FC = () => {
+  const [mode, setMode] = React.useState<Mode>("select");
+
   return (
     <Box
       boxShadow={useColorModeValue("md", "md-dark")}
       borderRadius="2xl"
-      py="8"
+      p="8"
     >
-      <Stack spacing="4" px="8">
-        <ConnectWalletWrapper>
-          <Button w="full">build something valuable</Button>
-        </ConnectWalletWrapper>
-      </Stack>
+      <Box>
+        {mode === "select" && (
+          <Stack>
+            <Button
+              w="full"
+              onClick={() => {
+                setMode("create");
+              }}
+            >
+              Create Streaming
+            </Button>
+            <Button
+              w="full"
+              onClick={() => {
+                setMode("view");
+              }}
+            >
+              View Streaming
+            </Button>
+          </Stack>
+        )}
+        {mode == "create" && (
+          <Stack>
+            <ConnectWalletWrapper>
+              <Button w="full">Start</Button>
+            </ConnectWalletWrapper>
+            <Button
+              w="full"
+              onClick={() => {
+                setMode("select");
+              }}
+            >
+              Back
+            </Button>
+          </Stack>
+        )}
+        {mode == "view" && (
+          <Stack>
+            <Button w="full">View</Button>
+            <Button
+              w="full"
+              onClick={() => {
+                setMode("select");
+              }}
+            >
+              Back
+            </Button>
+          </Stack>
+        )}
+      </Box>
     </Box>
   );
 };
---
+++ packages/frontend/src/types/livepeer.ts
@@ -0,0 +1,1 @@
+export type Mode = "select" | "create" | "view";
